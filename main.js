thorvg/thorvg.viewer

commit 8f1d41cfee8ef7ce71fa9c57c6043cba4dfbd827

removed highlight feature
this has been redundant since we removed the scene-tree.
we will revisit this as well.

main.js
@@ -91,19 +91,12 @@ class Player {
 	totalFrame = 0;
 	repeat = true;
 	playing = false;
-	highlighted = false;
 
 	flush() {
 		var context = this.canvas.getContext('2d');
 
 		//draw the content image first
 		context.putImageData(this.imageData, 0, 0);
-
-		//draw the highlight image later
-		if (this.highlighted) {
-			context.fillStyle = "#5a8be466";
-			context.fillRect(this.geomHighlight[0], this.geomHighlight[1], this.geomHighlight[2], this.geomHighlight[3]);
-		}
 	}
 
 	render() {
@@ -256,24 +249,6 @@ class Player {
 		}
 	}
 
-	highlight(id) {
-		this.highlighted = true;
-		this.geomHighlight= Float32Array.from(this.tvg.geometry(id));
-		//don't need to flush because the animation do refresh.
-		if (!this.playing) this.flush();
-	}
-
-	unhighlight() {
-		this.highlighted = false;
-		//don't need to flush because the animation do refresh.
-		if (!this.playing) this.flush();
-	}
-
-	setOpacity(id, opacity) {
-		this.tvg.opacity(id, opacity);
-		if (!this.playing) this.render();
-	}
-
 	constructor() {
 		this.tvg = new Module.TvgWasm();
 		this.canvas = document.getElementById("image-canvas");
@@ -282,10 +257,6 @@ class Player {
 }
 
 function initialize() {
-	window.addEventListener('dragenter', fileDropHighlight, false);
-	window.addEventListener('dragleave', fileDropUnhighlight, false);
-	window.addEventListener('dragover', fileDropHighlight, false);
-	window.addEventListener('drop', fileDropUnhighlight, false);
 	window.addEventListener('drop', (evt)=>{
 		fileDropOrBrowseHandle(evt.dataTransfer.files);
 	}, false);
@@ -324,19 +295,6 @@ function openFileBrowse() {
 function allowedFileExtension(filename) {
 	player.filetype = filename.split('.').pop();
 	return (player.filetype === "tvg") || (player.filetype === "svg") || (player.filetype === "json") || (player.filetype === "png") || (player.filetype === "jpg")
-}
-
-function fileDropHighlight(event) {
-	event.preventDefault();
-	event.stopPropagation();
-	event.dataTransfer.dropEffect = 'copy';
-	document.getElementById('image-area').classList.add("highlight");
-}
-
-function fileDropUnhighlight(event) {
-	event.preventDefault();
-	event.stopPropagation();
-	document.getElementById('image-area').classList.remove("highlight");
 }
 
 function fileDropOrBrowseHandle(files) {
@@ -614,15 +572,6 @@ function exportCanvasToPng() {
 		link.click();
 		document.body.removeChild(link);
 	}, 'image/png');
-}
-
-function highlight(event) {
-	var id = parseInt(this.getAttribute('tvg-id'));
-	player.highlight(id);
-}
-
-function unhighlight(event) {
-	player.unhighlight();
 }
 
 function deletePopup() {

commit 37e996c22df160eb650343e5fca682a62d4d4c19

Fixed a broken file drag & drop function
the bug caused by 8f1d41cfee8ef7ce71fa9c57c6043cba4dfbd827

main.js
@@ -283,6 +283,8 @@ class Player {
 }
 
 function initialize() {
+	window.addEventListener('dragover', fileDropHighlight, false);
+	window.addEventListener('drop', fileDropUnhighlight, false);
 	window.addEventListener('drop', (evt)=>{
 		fileDropOrBrowseHandle(evt.dataTransfer.files);
 	}, false);
@@ -321,6 +323,17 @@ function openFileBrowse() {
 function allowedFileExtension(filename) {
 	player.filetype = filename.split('.').pop();
 	return (player.filetype === "tvg") || (player.filetype === "svg") || (player.filetype === "json") || (player.filetype === "png") || (player.filetype === "jpg")
+}
+
+function fileDropHighlight(event) {
+	event.preventDefault();
+	event.stopPropagation();
+	event.dataTransfer.dropEffect = 'copy';
+}
+
+function fileDropUnhighlight(event) {
+	event.preventDefault();
+	event.stopPropagation();
 }
 
 function fileDropOrBrowseHandle(files) {